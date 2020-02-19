df30e7ca1ea456b4a7c4adfa75319732d2a4372a
Samuel Boulery <samuelboulery@gmail.com>
2020-02-19 15:15:27 +0100

    2 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+  #
+  # resources :reviews, only: [:show]
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # namespace :admin do
+  #   resources :restaurants, only: [:index]
+  # end
+  #
+  # resources :restaurants, only: [:index]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destruction of the DA-TA-BASE'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:         'belgian',
+    phone_number:  "22"
+  },
+  {
+    name:         'Kioku',
+    address:      '8 Boundary St, London E2 7JE',
+    category:         'japanese',
+    phone_number:  "2345"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
